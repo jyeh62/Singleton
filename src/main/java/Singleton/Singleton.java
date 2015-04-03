12a7931b5c176b8ce7062090afa602b7d5451970
jyeh <jyeh62@gmail.com>
2015-04-03 08:21:26 +0100

    gradle

---
+++ src/main/java/Singleton/Singleton.java
@@ -0,0 +1,20 @@
+public class Singleton {
+    //JVM에 의해 inner static class는 해당 class가 최초 사용 될 때
+    //(아래 예에선 getInstance 호출시) load됨
+    //따라서 getInstance동기화가 필요 없음(Initialization on Demand Holder)
+    private static class Helper{ 
+        static Singleton mInstance = new Singleton();
+        private Helper()
+        {
+            System.out.println("Helper create");
+        }
+    }
+
+    private Singleton(){
+        System.out.println("Singleton create");
+    }
+
+    public static Singleton getInstance(){
+        return Helper.mInstance; 
+    }
+}
