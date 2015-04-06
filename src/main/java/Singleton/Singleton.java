85cace69ea878a9afe5d50461e76db1a2daa5603
jyeh62 <jyeh62@gmail.com>
2015-04-06 15:43:48 +0900

    Update Singleton.java

--- src/main/java/Singleton/Singleton.java
+++ src/main/java/Singleton/Singleton.java
@@ -1,9 +1,9 @@
-public class Singleton {
+public final class Singleton {
     //JVM에 의해 inner static class는 해당 class가 최초 사용 될 때
     //(아래 예에선 getInstance 호출시) load됨
     //따라서 getInstance동기화가 필요 없음(Initialization on Demand Holder)
-    private static class Helper{ 
-        static Singleton mInstance = new Singleton();
+    private final static class Helper{ 
+        static final Singleton mInstance = new Singleton();
         private Helper()
         {
             System.out.println("Helper create");
